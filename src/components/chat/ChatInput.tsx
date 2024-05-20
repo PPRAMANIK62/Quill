33b348852a0261a277e032fa795c46042439a924
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-20 19:32:38 +0530

    FEAT: pdf render states in chat section done

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ChatINputProps {
+  isDisabled: boolean;
+}
+
+const ChatInput = ({ isDisabled }: ChatINputProps) => {
+  return <div>ChatInput</div>;
+};
+
+export default ChatInput;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -31,6 +31,7 @@ export const appRouter = router({
 
     return { success: true };
   }),
+
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
     const { userId } = ctx;
 
@@ -40,6 +41,22 @@ export const appRouter = router({
       },
     });
   }),
+
+  getFileUploadStatus: privateProcedure
+    .input(z.object({ fileId: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const file = await db.file.findFirst({
+        where: {
+          id: input.fileId,
+          userId: ctx.userId,
+        },
+      });
+
+      if (!file) return { status: "PENDING" as const };
+
+      return { status: file.uploadStatus };
+    }),
+
   getFile: privateProcedure
     .input(z.object({ key: z.string() }))
     .mutation(async ({ ctx, input }) => {

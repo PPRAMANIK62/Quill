762a080bdf97fd2b758ec060535ea20eb7479aa6
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-06-14 13:03:35 +0530

    ai messages streaming in

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,8 +1,9 @@
 import { trpc } from "@/app/_trpc/client";
 import { INFINITE_QUERY_LIMIT } from "@/config/infiniteQuery";
 import { Loader2, MessageSquare } from "lucide-react";
-import React from "react";
+import { useContext } from "react";
 import Skeleton from "react-loading-skeleton";
+import { ChatContext } from "./ChatContext";
 import Message from "./Message";
 
 interface MessagesProps {
@@ -10,6 +11,8 @@ interface MessagesProps {
 }
 
 const Messages = ({ fileId }: MessagesProps) => {
+  const { isLoading: isAiThinking } = useContext(ChatContext);
+
   const { data, isLoading, fetchNextPage } =
     trpc.getFileMessages.useInfiniteQuery(
       {
@@ -35,7 +38,7 @@ const Messages = ({ fileId }: MessagesProps) => {
   };
 
   const combinedMessages = [
-    ...(true ? [loadingMessage] : []),
+    ...(isAiThinking ? [loadingMessage] : []),
     ...(messages ?? []),
   ];
 
